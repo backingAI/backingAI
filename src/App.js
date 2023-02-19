b7efe4e4667ca543cef4c3af4f40599430175b03
backingAI <kaleemullah2932@gmail.com>
2023-02-19 14:13:13 +0500

    sheet 1 & sheet 2 with UI

--- src/App.js
+++ src/App.js
@@ -9,16 +9,25 @@ let nextId = 0;
 function App() {
   const [hello1, setHello1] = useState("");
   const [hello2, setHello2] = useState("");
+  const [sheet3, setSheet3] = useState("");
 
   const [isShown, setIsShown] = useState("");
 
   const handelClick = (event) => {
     setHello2(false);
+    setSheet3(false);
     setIsShown((current) => !current);
   };
   const handelClick2 = (event) => {
     setIsShown(false);
+    setSheet3(false);
     setHello2((current) => !current);
+  };
+
+  const handelClick3 = (event) => {
+    setIsShown(false);
+    setHello2(false);
+    setSheet3((current) => !current);
   };
 
   //var cityInfo = [];
@@ -28,11 +37,14 @@ function App() {
   const [towns, setTowns] = useState("");
   const [ucs, setucs] = useState("");
   const [population, setpopulation] = useState("");
-  const [totalUcs, setTotalUcs] = useState(0);
-  const [totalPopulation, setTotalPopulation] = useState(0);
   const [cityInfo1, setCityInfo1] = useState([]);
   const [mw, setmw] = useState(0);
   const [bw, setbw] = useState(0);
+
+  const [totalUcs, setTotalUcs] = useState(0);
+  const [totalPopulation, setTotalPopulation] = useState(0);
+  const [totalSWG, setTotalSWG] = useState(0);
+  const [totalMSW, settotalMSW] = useState(0);
 
   const handelSubmit = (event) => {
     event.preventDefault();
@@ -41,7 +53,6 @@ function App() {
     rowDict.towns = towns;
     rowDict.ucs = ucs;
     rowDict.population = population;
-    //cityInfo1.push(rowDict);
     setCityInfo1((arr) => [
       ...arr,
       {
@@ -50,14 +61,13 @@ function App() {
         towns: towns,
         ucs: ucs,
         population: population,
-        mw: mw,
-        bw: bw,
+        mw: 0,
+        bw: 0,
       },
     ]);
-    console.log(totalPopulation + parseInt(population));
-    console.log();
     setTotalPopulation(totalPopulation + parseInt(population));
     setTotalUcs(totalUcs + parseInt(ucs));
+    setTotalSWG(Math.round(totalPopulation / 0.54));
   };
 
   return (
@@ -66,10 +76,13 @@ function App() {
         <div className="div-menue">
           <h1 onClick={handelClick}>Basic City Information</h1>
           <h1 onClick={handelClick2}>Water Generation & major component</h1>
+          <h1 onClick={handelClick3}>Commercial Waste</h1>
         </div>
         {isShown && (
           <div className="div-content">
-            {/* <BasicInformation /> */}
+            <div>
+              <h1>Enter Zone Information</h1>
+            </div>
             <div>
               <div>
                 <label>
@@ -77,34 +90,27 @@ function App() {
                   Zone
                   <input
                     type="text"
-                    //   value={totalOrganicWaste}
                     onChange={(e) => setZone(e.target.value)}
                   />
                 </label>
                 <label>
                   {" "}
-                  towns
+                  Towns
                   <input
                     type="text"
-                    //   value={totalOrganicWaste}
                     onChange={(e) => setTowns(e.target.value)}
                   />
                 </label>
                 <label>
                   {" "}
-                  ucs
-                  <input
-                    type="text"
-                    //   value={totalOrganicWaste}
-                    onChange={(e) => setucs(e.target.value)}
-                  />
+                  UCS
+                  <input type="text" onChange={(e) => setucs(e.target.value)} />
                 </label>
                 <label>
                   {" "}
-                  population
+                  Population
                   <input
                     type="text"
-                    //   value={totalOrganicWaste}
                     onChange={(e) => setpopulation(e.target.value)}
                   />
                 </label>
@@ -113,16 +119,25 @@ function App() {
                   value="update"
                   onClick={handelSubmit}
                 >
-                  Calculate
+                  Submit
                 </button>
                 <div className="results">
-                  <ul>
-                    {cityInfo1.map((city) => (
-                      <li key={city.id}>
-                        {city.zone} {city.towns} {city.ucs} {city.population}
-                      </li>
-                    ))}
-                  </ul>
+                  {cityInfo1.map((city) => (
+                    <div className="result-inner">
+                      <div className="para-div">
+                        <p className="zone-info-p">{city.zone}</p>
+                      </div>
+                      <div className="para-div">
+                        <p className="zone-info-p"> {city.towns}</p>
+                      </div>
+                      <div className="para-div">
+                        <p className="zone-info-p">{city.ucs}</p>
+                      </div>
+                      <div className="para-div">
+                        <p className="zone-info-p">{city.population}</p>
+                      </div>
+                    </div>
+                  ))}
                 </div>
               </div>
               <div>
@@ -134,7 +149,89 @@ function App() {
         )}
         {hello2 && (
           <div className="div-content">
-            <h1>hello 2</h1>
+            {/* <div className="para-div-1">
+              <h3>Zone</h3>
+            </div>
+            <div className="para-div-1">
+              <h3>SWG(kg/d)</h3>
+            </div>
+            <div className="para-div-1">
+              <h3>SWG(t/d)</h3>
+            </div>
+            <div className="para-div-1">
+              <h3>%MW</h3>
+            </div>
+            <div className="para-div-1">
+              <h3>%BW</h3>
+            </div> */}
+            {/* <div className="sheet-2-main-dev"> */}
+            {cityInfo1.map((city) => (
+              <div className="sheet-2-inner-div">
+                <div className="para-div-1">
+                  <p className="zone-info-p">{city.zone}</p>
+                </div>
+                <div className="para-div-1">
+                  <p className="zone-info-p">
+                    SWG(Kg/d): {Math.round(city.population * 0.54)}
+                  </p>
+                </div>
+                <div className="para-div-1">
+                  <p className="zone-info-p">
+                    SWG(t/d): {Math.round((city.population * 0.54) / 1000)}
+                  </p>
+                </div>
+                <div className="para-div-1">
+                  <label>
+                    %MW
+                    <div className="para-div-1">
+                      <input
+                        type="text"
+                        onChange={(e) => {
+                          setmw(e.target.value);
+                          city.mw = e.target.value;
+                          city.bw =
+                            Math.round((1 - parseFloat(e.target.value)) * 100) /
+                            100;
+                          settotalMSW(
+                            totalMSW +
+                              Math.round(
+                                Math.round((city.population * 0.54) / 1000) *
+                                  city.bw
+                              )
+                          );
+                        }}
+                      />
+                    </div>
+                  </label>
+                </div>
+                <div className="para-div-1">
+                  <p className="zone-info-p">%MW: {city.mw}</p>
+                </div>
+                <div className="para-div-1">
+                  <p className="zone-info-p">%BW: {city.bw}</p>
+                </div>
+
+                <p>
+                  {Math.round(
+                    Math.round((city.population * 0.54) / 1000) * city.mw
+                  )}
+                </p>
+                <p>
+                  {Math.round(
+                    Math.round((city.population * 0.54) / 1000) * city.bw
+                  )}
+                </p>
+              </div>
+            ))}
+            {/* </div> */}
+            <p>Total SWG: {totalSWG}</p>
+            <p>Total SWG/1000: {Math.round(totalSWG / 1000)}</p>
+            <p>Total MSW: {totalMSW}</p>
+          </div>
+        )}
+        {sheet3 && (
+          <div className="div-content">
+            <h1>sheet 3</h1>
           </div>
         )}
       </div>
--- src/BasicInformation/BasicInformation.jsx
+++ src/BasicInformation/BasicInformation.jsx
@@ -36,68 +36,7 @@ function BasicInformation() {
     setTotalUcs(totalUcs + parseInt(ucs));
   };
 
-  return (
-    <>
-      <div>
-        <div>
-          <label>
-            {" "}
-            Zone
-            <input
-              type="text"
-              //   value={totalOrganicWaste}
-              onChange={(e) => setZone(e.target.value)}
-            />
-          </label>
-          <label>
-            {" "}
-            towns
-            <input
-              type="text"
-              //   value={totalOrganicWaste}
-              onChange={(e) => setTowns(e.target.value)}
-            />
-          </label>
-          <label>
-            {" "}
-            ucs
-            <input
-              type="text"
-              //   value={totalOrganicWaste}
-              onChange={(e) => setucs(e.target.value)}
-            />
-          </label>
-          <label>
-            {" "}
-            population
-            <input
-              type="text"
-              //   value={totalOrganicWaste}
-              onChange={(e) => setpopulation(e.target.value)}
-            />
-          </label>
-          <button className="submit-btn" value="update" onClick={handelSubmit}>
-            Calculate
-          </button>
-          <div className="results">
-            {/* <p>{totalUcs}</p>
-            <p>{totalPopulation}</p> */}
-            <ul>
-              <li></li>
-            </ul>
-            <ul>
-              {cityInfo1.map((city) => (
-                <li key={city.id}>
-                  {city.ucs} {city.population}
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-        <div></div>
-      </div>
-    </>
-  );
+  return <></>;
 }
 
 export default BasicInformation;
